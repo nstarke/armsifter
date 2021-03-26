f417ed05d24b6f4004f54870c3817b68b434bd43
nstarke <starke.nicholas@gmail.com>
2021-03-26 15:42:09 -0500

    Fixing start index tracking

--- armsifter.c
+++ armsifter.c
@@ -11,13 +11,15 @@
 #include <sys/wait.h>
 #include <sys/stat.h>
 
-#define HELLO_SIZE 312
 #define MEM_PATH "/proc/self/fd/%i"
 
+#define IDX_FILE "start_idx"
+
 int find_index(char *data, size_t len) {
+    size_t j = (len + (2 - 1)) / 2;
     // Magic OPCODE: e3a01000
-    for (int i = 0; i < len; i += 4) {
-        if (data[i] = 0x00 
+    for (int i = 0; i < j; i += 2) {
+        if (data[i] == 0x00 
         && data[i+1] == 0x10 
         && data[i+2] == 0xa0 
         && data[i+3] == 0xe3) {
@@ -25,6 +27,26 @@ int find_index(char *data, size_t len) {
         }
     }
 
+    return 0;
+}
+
+int check_idx_file() {
+    unsigned int file_data;
+    if (access(IDX_FILE, F_OK) == 0) {
+        FILE *f = fopen(IDX_FILE, "r");    
+        fread(&file_data, sizeof(int), 1, f);
+        fclose(f);
+        return file_data;
+    } else {
+        return -1;
+    }
+    
+}
+
+int write_idx_file(unsigned int idx) {
+    FILE * f = fopen(IDX_FILE, "w");
+    fwrite(&idx, sizeof(int), 1, f); 
+    fclose(f);
     return 0;
 }
 
@@ -48,7 +70,7 @@ int main(int argc, char * argv[]) {
         return 1;
     }
 
-    if (st.size == 0) {
+    if (st.st_size == 0) {
         printf("Assemble and link hello.S");
         return 1;
     }
@@ -61,27 +83,32 @@ int main(int argc, char * argv[]) {
         return 1;
     }
 
-    addr = mmap(NULL, st.size, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_SHARED, template, 0);
+    addr = mmap(NULL, st.st_size, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_SHARED, template, 0);
 
     if (addr == MAP_FAILED) {
         perror("mmap failed");
         return 1;
     }
 
-    idx = find_index(addr, st.size);
+    idx = check_idx_file();
+    if (idx == -1){
+        idx = find_index(addr, st.st_size);
 
-    if (idx == 0) {
-        printf("offset not found\n");
-        return 1;
+        if (idx == 0) {
+            printf("offset not found - assemble and link 'hello'?\n");
+            return 1;
+        }
+
+        write_idx_file(idx);
+        printf("No revious start index found - start index file written.\n");
+    } else {
+        printf("Previous start index found\n");
     }
 
-    printf("idx: %x", idx);
-
-    return 0;
+    printf("idx: %x\n", idx);
 
     sprintf(position_value, "%02X%02X%02X%02X", addr[idx+3], addr[idx+2], addr[idx+1], addr[idx]);
 
-    
     pos = strtoul(position_value, NULL, 16);
 
     if (argv[1]) {
@@ -110,7 +137,7 @@ int main(int argc, char * argv[]) {
                 }
                
                 memcpy(addr + 0x54, &i, 4);
-                status = write(mem_holder, addr, st.size);
+                status = write(mem_holder, addr, st.st_size);
                 if (status == -1) {
                     perror("write to memfd failed");
                     return 1;
@@ -119,10 +146,8 @@ int main(int argc, char * argv[]) {
                 sprintf(to_exec, MEM_PATH, mem_holder);
                 // printf("Executing: %s\n", to_exec);
                 if (execl(to_exec, NULL) == -1) {
-                    perror("execl");
-                }
-
-                printf("Executed\n");
+                   
+                } 
                 return 0;
             } else {
                 wait(NULL);
