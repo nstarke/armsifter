322a7d09b3ad29a0aa7226adb592a8389108e7cc
nstarke <starke.nicholas@gmail.com>
2021-03-26 15:40:30 -0500

    Adding start index tracking

--- armsifter.c
+++ armsifter.c
@@ -9,11 +9,27 @@
 #include <sys/syscall.h>
 #include <linux/memfd.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
 
 #define HELLO_SIZE 312
 #define MEM_PATH "/proc/self/fd/%i"
 
+int find_index(char *data, size_t len) {
+    // Magic OPCODE: e3a01000
+    for (int i = 0; i < len; i += 4) {
+        if (data[i] = 0x00 
+        && data[i+1] == 0x10 
+        && data[i+2] == 0xa0 
+        && data[i+3] == 0xe3) {
+            return i;
+        }
+    }
+
+    return 0;
+}
+
 int main(int argc, char * argv[]) {
+    int idx;
     int status;
     char *addr;
     int template;
@@ -24,6 +40,18 @@ int main(int argc, char * argv[]) {
     to_exec = malloc(32);
     char trial_value[5];
     pid_t child;
+    struct stat st;
+    status = stat("./hello", &st);
+
+    if (status != 0) {
+        perror("stat failed");
+        return 1;
+    }
+
+    if (st.size == 0) {
+        printf("Assemble and link hello.S");
+        return 1;
+    }
 
     printf("Begin Process\n");
 
@@ -33,14 +61,27 @@ int main(int argc, char * argv[]) {
         return 1;
     }
 
-    addr = mmap(NULL, HELLO_SIZE, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_SHARED, template, 0);
+    addr = mmap(NULL, st.size, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_SHARED, template, 0);
 
     if (addr == MAP_FAILED) {
         perror("mmap failed");
         return 1;
     }
 
-    sprintf(position_value, "%02X%02X%02X%02X", addr[0x57], addr[0x56], addr[0x55], addr[0x54]);
+    idx = find_index(addr, st.size);
+
+    if (idx == 0) {
+        printf("offset not found\n");
+        return 1;
+    }
+
+    printf("idx: %x", idx);
+
+    return 0;
+
+    sprintf(position_value, "%02X%02X%02X%02X", addr[idx+3], addr[idx+2], addr[idx+1], addr[idx]);
+
+    
     pos = strtoul(position_value, NULL, 16);
 
     if (argv[1]) {
@@ -69,7 +110,7 @@ int main(int argc, char * argv[]) {
                 }
                
                 memcpy(addr + 0x54, &i, 4);
-                status = write(mem_holder, addr, HELLO_SIZE);
+                status = write(mem_holder, addr, st.size);
                 if (status == -1) {
                     perror("write to memfd failed");
                     return 1;
